d16aa3b3a98206067d6b88815739a732d34a6ab4
Panhungting <a0910827893@gmail.com>
2019-07-19 14:39:00 +0900

    001

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0719_Git_up01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Git_add");
+            Console.ReadLine();
+        }
+    }
+}
